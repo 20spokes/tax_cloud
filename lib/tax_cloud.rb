b4dc6ed35cc633a87208420a82745ffdd8fa69c4
Ryan Fischer <ryan.fischer@20spokes.com>
2020-12-05 14:24:50 -0600

    Fixed issue with url not working

--- lib/tax_cloud.rb
+++ lib/tax_cloud.rb
@@ -27,7 +27,7 @@ I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales', 'en.yml
 # For information on configuring and using the TaxCloud API, look at the <tt>README</tt> file.
 module TaxCloud
   # WSDL location for TaxCloud API.
-  WSDL_URL = 'https://api.taxcloud.net/1.0/?wsdl'.freeze
+  WSDL_URL = 'https://api.taxcloud.com/1.0/taxcloud.asmx?wsdl'.freeze
 
   # TaxCloud API version.
   API_VERSION = '1.0'.freeze
